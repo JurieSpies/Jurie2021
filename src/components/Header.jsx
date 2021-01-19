78536442cb44af998a0d91a30f5f3701eb1c12ab
Jurie_OV <jurie.spies@openvantage.co.za>
2021-01-19 20:53:53 +0200

    adding title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,20 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-    <ParticlesBg num={100} type="cobweb" bg={true} />
-    <div>
-      Jurie Spies
-    </div>
+      <div style={styles.title}>
+        Jurie Spies
+      </div>
+      <ParticlesBg num={100} type="cobweb" bg={false}/>
     </div>
   )
 }
 
 export default Header
+
+const styles={
+  title:{
+    backgroundColor: 'pink', 
+    display:'flex',
+    alignSelf: 'center',
+  }
+}
