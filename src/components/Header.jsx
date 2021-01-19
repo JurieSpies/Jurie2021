fd6273391dc19d2eaa36aa69001be600ef4782cf
Jurie_OV <jurie.spies@openvantage.co.za>
2021-01-19 20:44:11 +0200

    Resume start

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import ParticlesBg from 'particles-bg'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+    <ParticlesBg num={100} type="cobweb" bg={true} />
+    <div>
+      Jurie Spies
+    </div>
+    </div>
+  )
+}
+
+export default Header
