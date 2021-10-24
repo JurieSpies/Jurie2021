60af646cfeee7dd241e0fb7b162e718a6858a110
Jurie_OV <jurie.spies@openvantage.co.za>
2021-10-24 04:46:02 +0200

    removed mouse particles

--- src/pages/AnimatedBackground.jsx
+++ src/pages/AnimatedBackground.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import MouseParticles from 'react-mouse-particles';
+// import MouseParticles from 'react-mouse-particles';
 import Particles from 'react-particles-js';
 
 const styles = {
@@ -17,7 +17,7 @@ const styles = {
 
 const AnimatedBackground = () => (
   <div style={styles.mainContainer}>
-    <MouseParticles g={3} color="white" cull="col,image-wrapper" />
+    {/* <MouseParticles g={3} color="white" cull="col,image-wrapper" /> */}
     <Particles params={{ particles: { number: { value: 20 }, move: { speed: 1 } } }} />
   </div>
 );
