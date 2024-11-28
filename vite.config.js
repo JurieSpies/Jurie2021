e3482647d5bcb53233f0037b9fa6ece239796e8c
Jurie Spies <juriespies@gmail.com>
2024-11-28 19:00:53 +0200

    index update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,14 @@ export default defineConfig({
     alias: {
       '@': '/src',
     },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+    },
+    outDir: 'dist',
+    sourcemap: true,
   },
 });
